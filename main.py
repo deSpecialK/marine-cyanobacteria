d14866fbc24dd62bed42a91b4b8e66d919c92458
Karsten <voigt@bio.uni-freiburg.de>
2025-12-19 17:04:44 +0100

    added load_data function

--- main.py
+++ main.py
@@ -6,16 +6,45 @@ This script analyses similarities of marine cyanobacteria in different seas of t
 Authors: Cosmima Schilling, Julian Weber, Sofia Kasek and Karsten Voigt
 """
 
-# load Data from files
-def loadData():
-    return
+# modules
+
+
+# Variables definitions 
+
+# load Data from files, define separator if needed, delete header if needed
+def load_data(file_handle,sep=None, header=False):
+	ohandle    = open(file_handle, 'r')
+	obuffer    = ohandle.readlines()
+	ohandle.close()
+	file_data   = []
+	head_info   = ""
+	if sep:
+		for i in obuffer:
+			if header:
+				header   = False
+				head_info = i
+				continue
+			atoms = string.split(i.strip("\n"), sep)
+			file_data.append(atoms)
+	else:
+		for i in obuffer:
+			if header:
+				header = False
+				head_info = i
+				continue
+			file_data.append(i.strip("\n"))
+	return file_data, head_info
 
 # Compare Data sets
-def compareData():
+def compareData():    
     return
 
 # write Data
 def writeData():
+
     return
 
 ### MAIN ###
+loadData()
+compareData()
+writeData()
