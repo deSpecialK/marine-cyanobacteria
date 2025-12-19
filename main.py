c73a6bb6c61b49a4bf2f673cb75aab098cc679ff
Karsten <voigt@bio.uni-freiburg.de>
2025-12-19 16:22:50 +0100

    python script skelett

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This script analyses similarities of marine cyanobacteria in different seas of the world
+Authors: Cosmima Schilling, Julian Weber, Sofia Kasek and Karsten Voigt
+"""
+
+# load Data from files
+def loadData():
+    return
+
+# Compare Data sets
+def compareData()
+    
+
+### MAIN ###
